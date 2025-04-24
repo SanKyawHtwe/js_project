ed9ad7c53924d1f6f35910c5e4a3a9dabe32c8fe
sankyawhtwe <sankyawhtwe.personal@gmail.com>
2025-04-24 23:00:32 +0630

    Add student model with schema definition for registration

---
+++ models/student.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  batch: {
+    type: String,
+    required: true,
+  },
+  enrolledDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+export default Student;
